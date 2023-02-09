8f99e31edea769570d7d748e349a4c07f09f3c57
Assadi <62776480+assadi-dev@users.noreply.github.com>
2023-02-09 09:21:20 +0100

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,25 @@
+$(document).ready(() => {
+  const generatePicture = (src, alt) => {
+    let figure = document.createElement("figure");
+    let img = document.createElement("img");
+    $(img).attr({
+      src,
+      alt,
+    });
+    $(img).prependTo(figure);
+    return figure;
+  };
+
+  $("#show").on("click", function (e) {
+    fetch("../data/data.json")
+      .then((res) => res.json())
+      .then((res) => {
+        res.map((data) => {
+          let li = document.createElement("li");
+          let img = generatePicture(data.path, data.name);
+          $(img).appendTo(li);
+          $(li).appendTo("#car-list");
+        });
+      });
+  });
+});
